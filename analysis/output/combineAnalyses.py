fb0a429e2c1e1e33a8e69f3bd5feeea3cef608d7
Annika Shankwitz <annikashankwitz@Zelda-7.local>
2025-11-29 16:13:52 -0500

    three decimal places

--- analysis/output/combineAnalyses.py
+++ analysis/output/combineAnalyses.py
@@ -16,7 +16,7 @@ simpleClean = simpleClean.rename(columns={-1: "-gap effect", 1: "+gap effect"})
 simpleClean["effectDifference"] =(simpleClean['-gap effect'] - simpleClean["+gap effect"]).abs()
 simpleClean["-gap direction"] = simpleClean["-gap effect"].apply(lambda x: "p" if x > 0 else ("n" if x < 0 else 'zero'))
 simpleClean["+gap direction"] = simpleClean["+gap effect"].apply(lambda x: "p" if x > 0 else ("n" if x < 0 else 'zero'))
-simpleClean.to_csv(f"{output}/simpleFGE.tsv", sep="\t")
+simpleClean.to_csv(f"{output}/simpleFGE.tsv", sep="\t", float_format="%.3f")
 
 #add the lme infor
 simpleCleanWithLME = simpleClean.merge(
@@ -27,11 +27,11 @@ simpleCleanWithLME = simpleClean.merge(
 
 #add info about the estimate direction
 simpleCleanWithLME["estimateDirection"] = simpleCleanWithLME["estimate"].apply(lambda x: "p" if x > 0 else ("n" if x < 0 else 'zero'))
-simpleCleanWithLME.to_csv(f"{output}/simpleAllInfo.tsv", sep="\t")
+simpleCleanWithLME.to_csv(f"{output}/simpleAllInfo.tsv", sep="\t", float_format="%.3f")
 
 #filter out the inccorrect lines
 simpleCorrect = simpleCleanWithLME[(simpleCleanWithLME["-gap direction"] == "p") & (simpleCleanWithLME["+gap direction"] == "n") & (simpleCleanWithLME["estimateDirection"] == "n") & (simpleCleanWithLME["sig"].str.match("^\*{1,3}$"))]
-simpleCorrect.to_csv(f"{output}/simpleCorrect.tsv", sep="\t")
+simpleCorrect.to_csv(f"{output}/simpleCorrect.tsv", sep="\t", float_format="%.3f")
 
 
 
@@ -48,7 +48,7 @@ islandClean = islandClean.rename(columns={-1: "-gap effect", 1: "+gap effect"})
 islandClean["effectDifference"] = (islandClean['-gap effect'] - islandClean["+gap effect"]).abs()
 islandClean["-gap direction"] = islandClean["-gap effect"].apply(lambda x: "p" if x > 0 else ("n" if x < 0 else 'zero'))
 islandClean["+gap direction"] = islandClean["+gap effect"].apply(lambda x: "p" if x > 0 else ("n" if x < 0 else 'zero'))
-islandClean.to_csv(f"{output}/islandFGE.tsv", sep="\t")
+islandClean.to_csv(f"{output}/islandFGE.tsv", sep="\t", float_format="%.3f")
 
 #add the lme infor
 islandCleanWithLME = islandClean.merge(
@@ -59,9 +59,9 @@ islandCleanWithLME = islandClean.merge(
 
 #add info about the estimate direction
 islandCleanWithLME["estimateDirection"] = islandCleanWithLME["estimate"].apply(lambda x: "p" if x > 0 else ("n" if x < 0 else 'zero'))
-islandCleanWithLME.to_csv(f"{output}/islandAllInfo.tsv", sep="\t")
+islandCleanWithLME.to_csv(f"{output}/islandAllInfo.tsv", sep="\t", float_format="%.3f")
  
 #filter out the inccorrect lines
 islandCorrect = islandCleanWithLME[(simpleCleanWithLME["-gap direction"] == "p") & (islandCleanWithLME["+gap direction"] == "n") & (islandCleanWithLME["estimateDirection"] == "p") & (islandCleanWithLME["sig"].str.match("^\*{1,3}$"))]
-islandCorrect.to_csv(f"{output}/islandCorrect.tsv", sep="\t")
+islandCorrect.to_csv(f"{output}/islandCorrect.tsv", sep="\t", float_format="%.3f")
 
